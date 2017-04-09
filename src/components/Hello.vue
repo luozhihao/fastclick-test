f893e3952ed682fc7db539bb88b077a74e83af64
chris <544095700@qq.com>
2017-04-09 19:12:59 +0800

    update

---
+++ src/components/Hello.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li><div @touchstart="addFn">点击</div></li>
+      <li><div @click="addFn">点击</div></li>
+      <li><div @click="addFn">点击</div></li>
+      <li><div @click="addFn">点击</div></li>
+      <li>2</li>
+      <li>2</li>
+      <li>2</li>
+      <li>2</li>
+      <li>2</li>
+      <li>2</li>
+      <li>2</li>
+      <li>2</li>
+      <li>2</li>
+      <li>2</li>
+      <li>2</li>
+      <li>2</li>
+      <li>2</li>
+      <li>2</li>
+      <li>2</li>
+      <li>2</li>
+      <li>2</li>
+      <li>2</li>
+      <li>2</li>
+      <li>2</li>
+      <li>2</li>
+      <li>2</li>
+      <li>2</li>
+      <li>2</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      init: {
+        num: 0
+      }
+    }
+  },
+  beforeMount() {
+
+  },
+  computed: {
+    msg() {
+      return this.init.num
+    }
+  },
+  methods: {
+    addFn() {
+      this.init.num += 1;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
